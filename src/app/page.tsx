689b733cb0be03f15b80775e708e64ab2d0736f0
RAVISHANKAR PAL <65111478+iravishankarpal@users.noreply.github.com>
2025-05-07 11:53:45 +0530

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import styles from "@/styles/home.module.css";
 
 export default function Home() {
-    return <div className={styles.container}>Hello</div>;
+  return <div className={styles.container}>Hello</div>;
 }
